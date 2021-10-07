9ff484305f98dc9cc6ed630f99073cae60a2a9aa
ummerukaya <ummesuny0530@gmail.com>
2021-10-07 19:48:21 +0600

    changed

---
+++ encapsulation/encapsulation/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class SwordDamage
+{
+    public const int BASE_DAMAGE = 3;
+    public const int FLAME_DAMAGE = 2;
+    public int Roll;
+    private decimal MagicMultiplier = 1M;
+    private int FlamingDamage = 0;
+    public int Damage;
+    private void CalculateDamage()
+    {
+        Damage = (int)(Roll * MagicMultiplier) + BASE_DAMAGE + FlamingDamage;
+        Console.WriteLine(Damage);
+    }
+    public void SetMagic(bool isMagic)
+    {
+        if (isMagic)
+        {
+            MagicMultiplier = 1.75M;
+        }
+        else
+        {
+            MagicMultiplier = 1M;
+        }
+        CalculateDamage();
+    }
+    public void SetFlaming(bool isFlaming)
+    {
+        CalculateDamage();
+        if (isFlaming)
+        {
+            Damage += FLAME_DAMAGE;
+        }
+    }
+
+}
+
+class main
+{ 
+    public static void Main(string[] args)
+    {
+        SwordDamage obj = new SwordDamage();
+        obj.SetMagic(true);
+        obj.SetFlaming(true);
+    }
+}
+
+
---
+++ polymorphism/polymorphism/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace polymorphism
+{
+    public class Calculate
+    {
+        public void AddNumbers(int a, int b)
+        {
+            Console.WriteLine("a + b = {0}", a + b);
+        }
+        public void AddNumbers(int a, int b, int c)
+        {
+            Console.WriteLine("a + b + c = {0}", a + b + c);
+        }
+
+    }
+
+    public class pClass
+    {
+        public virtual void hair()
+        {
+            Console.WriteLine("brown");
+        }
+    }
+    public class cClass : pClass
+    {
+        public override void hair()
+        {
+            Console.WriteLine("black");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate c = new Calculate();
+
+            //compile time polymorphism
+            c.AddNumbers(1, 2);
+            c.AddNumbers(1, 2, 3);
+
+            //runtime polymorphism
+            cClass d = new cClass();
+            d.hair();
+            pClass b = new pClass();
+            b.hair();
+
+        
+        }
+    }
+}
