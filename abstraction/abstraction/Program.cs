bb8cf70239ff810966245a2fd2addde11967d6ae
ummerukaya <ummesuny0530@gmail.com>
2021-10-07 12:58:39 +0600

    Initial Commit

---
+++ abstraction/abstraction/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public abstract class animal
+{
+    public abstract void animalSound();
+}
+
+public class cow : animal
+{
+    public override void animalSound()
+    {
+        Console.WriteLine("hamba");
+    }
+}
+
+public class cat : animal
+{
+    public override void animalSound()
+    {
+        Console.WriteLine("meow");
+    }
+}
+
+public class main_method
+{
+    public static void Main()
+    {
+        animal obj;
+
+        obj = new cow();
+        obj.animalSound();
+
+        obj = new cat();
+        obj.animalSound();
+
+    }
+}
+
+
---
+++ interface/interface/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+
+interface animal
+{
+    void animalSound();
+}
+
+class cow : animal
+{
+    public void animalSound()
+    {
+        Console.WriteLine("hamba");
+    }
+
+    public static void Main()
+    {
+        cow obj = new cow();
+
+        obj.animalSound();
+
+    }
+}
